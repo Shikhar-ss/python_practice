d4f60411e85fe986ec022f37a2a687db314c3f72
shikhar sharma <shikhar.sharma.ss.24@gmail.com>
2024-01-08 12:48:40 +0000

    all scenarios

--- programing_practice/bomb_diffusal.py
+++ programing_practice/bomb_diffusal.py
@@ -2,32 +2,28 @@ class Solution:
     def difffuse(self,code: list[int], k : int) -> [int]:
         n = len(code)
         result= []
-        if k >= 0:
-            print('Run formward loop')
+        if k > 0:
             for i in range(0,n):
                 sum = 0
                 for j in range (i+1,i+k+1):
                     sum += code[j % n]
                 result.append(sum)
-            print(result)
-                    
-        if k <= 0:
-            print('Run backward loop')
+        if k < 0:
             for i in range(0,n):
                 sum = 0
                 for j in range(i-1,i+k-1,-1):
                     sum += code[j % n]
                 result.append(sum)
-            print(result)
-            # return code[i] == sum
-            # if k == 0:
-            #     print('replace with 0') 
-            #     pass
-            # result.append(sum)
+        elif k == 0:
+            for i in range(0,n):
+                result.append(0)
+        
+        return result
+            
 
 
 code = [5,7,1,4]
-k = -3
+k = 3
 
 sol = Solution()
 sol.difffuse(code,k)
